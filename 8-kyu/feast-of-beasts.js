bfce65a11ff66a33506d9d6ef09f3d959e664eae
curm90 <liam_sutton1990@hotmail.co.uk>
2019-02-19 19:07:24 +0000

    more 8 kyus (19-02-19)

---
+++ 8-kyu/feast-of-beasts.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/the-feast-of-many-beasts/train/javascript
+
+function feast(beast, dish) {
+  
+  if (beast[0] === dish[0] 
+  && beast[beast.length - 1] === dish[dish.length - 1]) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function feast(beast, dish) {
+  return beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1]
+}
+
+console.log(feast("great blue heron", "garlic naan"), true)
+console.log(feast("chickadee", "chocolate cake"), true)
+console.log(feast("brown bear", "bear claw"), false)
