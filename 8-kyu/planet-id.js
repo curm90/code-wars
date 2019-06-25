1f45666afca43db7d712f7e80dddf816ab2b39e8
curm90 <liam_sutton1990@hotmail.co.uk>
2019-06-25 18:02:04 +0100

    add 25-06-19 solutions

---
+++ 8-kyu/planet-id.js
@@ -0,0 +1,48 @@
+// https://www.codewars.com/kata/get-planet-name-by-id/train/javascript
+
+function getPlanetName(id){
+  let name;
+  switch(id){
+    case 1:
+      name = 'Mercury'
+      break;
+    case 2:
+      name = 'Venus'
+      break;
+    case 3:
+      name = 'Earth'
+      break;
+    case 4:
+      name = 'Mars'
+      break;
+    case 5:
+      name = 'Jupiter'
+      break;
+    case 6:
+      name = 'Saturn'
+      break;
+    case 7:
+      name = 'Uranus'
+      break;
+    case 8:
+      name = 'Neptune'
+  }
+  return name;
+}
+
+function getPlanetName(id){
+  return {
+    1: 'Mercury',
+    2: 'Venus',
+    3: 'Earth',
+    4: 'Mars',
+    5: 'Jupiter',
+    6: 'Saturn',
+    7: 'Uranus',
+    8: 'Neptune'
+  }[id]
+}
+
+console.log(getPlanetName(2), 'Venus');
+console.log(getPlanetName(5), 'Jupiter');
+console.log(getPlanetName(3), 'Earth');
---
+++ 8-kyu/return-to-sanity.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/return-to-sanity/train/javascript
+
+function mystery() {
+  const results = {sanity: 'Hello'};
+  return results;
+}
+
+console.log(mystery(), {sanity: 'Hello'}, 'Mystery has not returned to sanity');
