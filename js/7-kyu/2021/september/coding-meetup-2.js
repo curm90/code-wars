6b9f43f6ee6c251b46bac033c3f0fb26750c3e86
Liam Sutton <devliam90@gmail.com>
2021-09-18 12:10:55 +0100

    coding meetup #2

---
+++ js/7-kyu/2021/september/coding-meetup-2.js
@@ -0,0 +1,68 @@
+// https://www.codewars.com/kata/58279e13c983ca4a2a00002a/train/javascript
+
+function greetDevelopers(list) {
+  return list.map((item) => {
+    item.greeting = `Hi ${item.firstName}, what do you like the most about ${item.language}?`;
+
+    return item;
+  });
+}
+
+var list1 = [
+  {
+    firstName: 'Sofia',
+    lastName: 'I.',
+    country: 'Argentina',
+    continent: 'Americas',
+    age: 35,
+    language: 'Java',
+  },
+  {
+    firstName: 'Lukas',
+    lastName: 'X.',
+    country: 'Croatia',
+    continent: 'Europe',
+    age: 35,
+    language: 'Python',
+  },
+  {
+    firstName: 'Madison',
+    lastName: 'U.',
+    country: 'United States',
+    continent: 'Americas',
+    age: 32,
+    language: 'Ruby',
+  },
+];
+
+var answer = [
+  {
+    firstName: 'Sofia',
+    lastName: 'I.',
+    country: 'Argentina',
+    continent: 'Americas',
+    age: 35,
+    language: 'Java',
+    greeting: 'Hi Sofia, what do you like the most about Java?',
+  },
+  {
+    firstName: 'Lukas',
+    lastName: 'X.',
+    country: 'Croatia',
+    continent: 'Europe',
+    age: 35,
+    language: 'Python',
+    greeting: 'Hi Lukas, what do you like the most about Python?',
+  },
+  {
+    firstName: 'Madison',
+    lastName: 'U.',
+    country: 'United States',
+    continent: 'Americas',
+    age: 32,
+    language: 'Ruby',
+    greeting: 'Hi Madison, what do you like the most about Ruby?',
+  },
+];
+
+console.log(greetDevelopers(list1), answer);
