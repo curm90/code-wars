3a397ead9affba6fa773a0adb5addf66a5d1cada
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2022-06-18 14:22:46 +0100

    largest square inside circle

---
+++ js/7-kyu/2022/june/largest-square-inside-circle.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/5887a6fe0cfe64850800161c/train/javascript
+
+function areaLargestSquare(r) {
+  return r * r * 2;
+}
+
+function areaLargestSquare(r) {
+  return 2 * r ** 2;
+}
+
+console.log(areaLargestSquare(5), 50);
+console.log(areaLargestSquare(7), 98);
+console.log(areaLargestSquare(15), 450);
