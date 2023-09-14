71259666c748fdde2db9a5947efd483d743e1b41
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2023-09-14 17:07:12 +0100

    number to string

---
+++ rust/8-kyu/september/num-to-str.rs
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/rust
+
+fn number_to_string(i: i32) -> String {
+    i.to_string()
+}
+
+fn number_to_string(i: i32) -> String {
+    format!("{}", i)
+}
