87a0861dbcda0fea25e48622d8ed362f7aab1221
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2025-06-18 05:38:21 +0100

    cleanup

--- js/6-kyu/2025/june/mexican-wave.js
+++ js/6-kyu/2025/june/mexican-wave.js
@@ -13,35 +13,4 @@ function wave(str) {
   return result;
 }
 
-let result = ['Hello', 'hEllo', 'heLlo', 'helLo', 'hellO'];
-console.log(wave('hello'), result, "Should return: '" + result + "'");
-
-// result = [
-//   'Codewars',
-//   'cOdewars',
-//   'coDewars',
-//   'codEwars',
-//   'codeWars',
-//   'codewArs',
-//   'codewaRs',
-//   'codewarS',
-// ];
-// // console.log(wave('codewars'), result, "Should return: '" + result + "'");
-
-// // result = [];
-// // console.log(wave(''), result, "Should return: '" + result + "'");
-
-// // result = [
-// //   'Two words',
-// //   'tWo words',
-// //   'twO words',
-// //   'two Words',
-// //   'two wOrds',
-// //   'two woRds',
-// //   'two worDs',
-// //   'two wordS',
-// // ];
-// // console.log(wave('two words'), result, "Should return: '" + result + "'");
-
-// // result = [' Gap ', ' gAp ', ' gaP '];
-// // console.log(wave(' gap '), result, "Should return: '" + result + "'");
+console.log(wave('hello'), ['Hello', 'hEllo', 'heLlo', 'helLo', 'hellO']);
