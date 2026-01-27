b0c919a7afb82a8a11cf288a0c74af1f82189964
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2026-01-27 19:50:40 +0000

    first pass solution for all inclusive

---
+++ js/7-kyu/2026/january/all-inclusive.js
@@ -0,0 +1,46 @@
+// https://www.codewars.com/kata/5700c9acc1555755be00027e/train/javascript
+
+function containAllRots(str, arr) {
+  // if no str input return true
+  if (!str) return true;
+
+  // place to store a counter
+  let i = 0;
+  const strArr = str.split('');
+
+  let temp = null;
+  // iterate while i is less than str.length
+  while (i < str.length) {
+    // if current rotation of str is not in the arr
+    if (!arr.includes(strArr.join(''))) {
+      // return false
+      return false;
+    }
+
+    // else we need to rotate the string
+    const firstLetter = strArr.splice(0, 1)[0];
+    strArr.push(firstLetter);
+    // increment i
+    i += 1;
+  }
+
+  // after all iterations if we didnt return it means all rotations were in the array and we can return true
+  return true;
+}
+
+console.log(containAllRots('', []), true);
+console.log(containAllRots('', ['bsjq', 'qbsj']), true);
+console.log(containAllRots('bsjq', ['bsjq', 'qbsj', 'sjqb', 'twZNsslC', 'jqbs']), true);
+console.log(
+  containAllRots('XjYABhR', [
+    'TzYxlgfnhf',
+    'yqVAuoLjMLy',
+    'BhRXjYA',
+    'YABhRXj',
+    'hRXjYAB',
+    'jYABhRX',
+    'XjYABhR',
+    'ABhRXjY',
+  ]),
+  false,
+);
