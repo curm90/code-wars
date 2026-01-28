ed518cc2718313b038516ffd17e40e2d69f1c5dd
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2026-01-28 07:38:58 +0000

    2nd pass solution for all inclusive using a set

--- js/7-kyu/2026/january/all-inclusive.js
+++ js/7-kyu/2026/january/all-inclusive.js
@@ -8,7 +8,6 @@ function containAllRots(str, arr) {
   let i = 0;
   const strArr = str.split('');
 
-  let temp = null;
   // iterate while i is less than str.length
   while (i < str.length) {
     // if current rotation of str is not in the arr
@@ -25,6 +24,21 @@ function containAllRots(str, arr) {
   }
 
   // after all iterations if we didnt return it means all rotations were in the array and we can return true
+  return true;
+}
+
+function containAllRots(str, arr) {
+  if (!str) return true;
+
+  const arrSet = new Set(arr);
+
+  for (let i = 0; i < str.length; i++) {
+    const rotation = str.slice(i) + str.slice(0, i);
+    if (!arrSet.has(rotation)) {
+      return false;
+    }
+  }
+
   return true;
 }
 
