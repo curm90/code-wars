e52134f11cef9f8f1ee9ce07c22868c1259ba2b3
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2026-01-29 10:26:26 +0000

    first pass solution for changing letter

---
+++ js/7-kyu/2026/january/changing-letter.js
@@ -0,0 +1,22 @@
+// https://www.codewars.com/kata/5831c204a31721e2ae000294/train/javascript
+
+function swap(string) {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+  return string
+    .split('')
+    .map((char) => (vowels.includes(char.toLowerCase()) ? char.toUpperCase() : char))
+    .join('');
+}
+
+console.log(swap(''), '');
+console.log(swap('   @@@'), '   @@@');
+console.log(swap('HelloWorld!'), 'HEllOWOrld!');
+console.log(swap('Sunday'), 'SUndAy');
+console.log(swap('Codewars'), 'COdEwArs');
+console.log(swap('Monday'), 'MOndAy');
+console.log(swap('Friday'), 'FrIdAy');
+console.log(swap('abracadabra'), 'AbrAcAdAbrA');
+console.log(swap('AbrAcAdAbrA'), 'AbrAcAdAbrA');
+console.log(swap('ABRACADABRA'), 'ABRACADABRA');
+console.log(swap('aBRaCaDaBRa'), 'ABRACADABRA');
