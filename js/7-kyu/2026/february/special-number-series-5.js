014aa765d98ea8fbdabcd10f34accf1a216745f3
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2026-02-09 12:41:34 +0000

    first pass loop solution

---
+++ js/7-kyu/2026/february/special-number-series-5.js
@@ -0,0 +1,22 @@
+// https://www.codewars.com/kata/5a55f04be6be383a50000187/train/javascript
+
+function specialNumber(n) {
+  const numArr = n.toString().split('');
+
+  for (const num of numArr) {
+    if (num > 5) return 'NOT!!';
+  }
+
+  return 'Special!!';
+}
+
+console.log(specialNumber(2), 'Special!!');
+console.log(specialNumber(3), 'Special!!');
+console.log(specialNumber(6), 'NOT!!');
+console.log(specialNumber(9), 'NOT!!');
+console.log(specialNumber(11), 'Special!!');
+console.log(specialNumber(55), 'Special!!');
+console.log(specialNumber(26), 'NOT!!');
+console.log(specialNumber(92), 'NOT!!');
+console.log(specialNumber(25432), 'Special!!');
+console.log(specialNumber(2783), 'NOT!!');
