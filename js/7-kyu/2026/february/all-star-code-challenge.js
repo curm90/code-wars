4db0bfb6d7fcf568f28a325f56d60c058ee13661
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2026-02-21 06:18:44 +0000

    remove uneccessary calc

--- js/7-kyu/2026/february/all-star-code-challenge.js
+++ js/7-kyu/2026/february/all-star-code-challenge.js
@@ -8,6 +8,13 @@ function toTime(seconds) {
   return `${Math.floor(hours)} hour(s) and ${Math.floor(minutes)} minute(s)`;
 }
 
+function toTime(seconds) {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+
+  return `${hours} hour(s) and ${minutes} minute(s)`;
+}
+
 console.log(toTime(3600), '1 hour(s) and 0 minute(s)');
 console.log(toTime(3601), '1 hour(s) and 0 minute(s)');
 console.log(toTime(3500), '0 hour(s) and 58 minute(s)');
