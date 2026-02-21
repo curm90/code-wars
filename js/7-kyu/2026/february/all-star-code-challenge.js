814bd2432f4b145cd66368fc5ca879b1e148f1f4
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2026-02-21 06:16:27 +0000

    first pass

---
+++ js/7-kyu/2026/february/all-star-code-challenge.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/5865cff66b5699883f0001aa/train/javascript
+
+function toTime(seconds) {
+  const hours = seconds / 3600;
+  const remainderAfterHours = seconds % 3600;
+  const minutes = remainderAfterHours / 60;
+
+  return `${Math.floor(hours)} hour(s) and ${Math.floor(minutes)} minute(s)`;
+}
+
+console.log(toTime(3600), '1 hour(s) and 0 minute(s)');
+console.log(toTime(3601), '1 hour(s) and 0 minute(s)');
+console.log(toTime(3500), '0 hour(s) and 58 minute(s)');
+console.log(toTime(323500), '89 hour(s) and 51 minute(s)');
+console.log(toTime(0), '0 hour(s) and 0 minute(s)');
