881184a0a94c7b71b70f44c3f9217bb3742d40e1
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2026-02-22 06:34:46 +0000

    improved solutions

--- js/7-kyu/2026/february/find-a-meeting-room.js
+++ js/7-kyu/2026/february/find-a-meeting-room.js
@@ -4,6 +4,24 @@ function meeting(x) {
   return x.indexOf('O') > -1 ? x.indexOf('O') : 'None available!';
 }
 
+function meeting(x) {
+  const index = x.indexOf('O');
+  return index > -1 ? index : 'None Available!';
+}
+
+function meeting(x) {
+  const index = x.findIndex((room) => room === 'O');
+  return index !== -1 ? index : 'None available!';
+}
+
+function meeting(x) {
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] === 'O') return i;
+  }
+
+  return 'None available!';
+}
+
 console.log(meeting(['X', 'O', 'X']), 1);
 console.log(meeting(['O', 'X', 'X', 'X', 'X']), 0);
 console.log(meeting(['X', 'X', 'X', 'X', 'X']), 'None available!');
