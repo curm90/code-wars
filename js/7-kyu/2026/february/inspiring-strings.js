9d1c2b172cc191974053065426de5f59c9d7f752
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2026-02-25 06:32:16 +0000

    first pass

---
+++ js/7-kyu/2026/february/inspiring-strings.js
@@ -0,0 +1,22 @@
+// https://www.codewars.com/kata/5939ab6eed348a945f0007b2/train/javascript
+
+function longestWord(stringOfWords) {
+  let result = '';
+  const strArr = stringOfWords.split(' ');
+
+  for (let i = strArr.length - 1; i >= 0; i--) {
+    const currentWord = strArr[i];
+
+    if (currentWord.length > result.length) {
+      result = currentWord;
+    }
+  }
+
+  return result;
+}
+
+console.log(longestWord('lonely'), 'lonely');
+console.log(longestWord('a pair'), 'pair');
+console.log(longestWord('a b c d e fgh'), 'fgh');
+console.log(longestWord('one two three'), 'three');
+console.log(longestWord('red blue grey'), 'grey');
